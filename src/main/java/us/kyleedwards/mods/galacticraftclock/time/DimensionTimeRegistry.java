9b045fabd7e70018b4e7a9ccf3ab7d1c2593f5c0
Kyle Edwards <kyleedwardsny@gmail.com>
2017-11-15 00:32:31 -0500

    Updating times on the client caused problems when pausing; once per second should be enough, so only update when receiving a message from server

--- src/main/java/us/kyleedwards/mods/galacticraftclock/time/DimensionTimeRegistry.java
+++ src/main/java/us/kyleedwards/mods/galacticraftclock/time/DimensionTimeRegistry.java
@@ -7,14 +7,6 @@ public class DimensionTimeRegistry
 {
     public static HashMap<Integer, DimensionTimeInfo> dimensionTimes = new HashMap<Integer, DimensionTimeInfo>();
 
-    public static void doTick()
-    {
-        for (Map.Entry<Integer, DimensionTimeInfo> entry : dimensionTimes.entrySet())
-        {
-            entry.getValue().time++;
-        }
-    }
-
     public static void setDimensionTimes(Map<Integer, DimensionTimeInfo> times)
     {
         dimensionTimes.clear();
